cde2b5b40ff3f774e6aca376eeaa6a565f4488f7
dkim19375 <miner8mk@gmail.com>
2020-11-25 11:13:26 -0500

    Revert "UNTESTED"
    This reverts commit d7c13c0c4b6308d93c39a683a8c62551aa6ac8cc.

--- src/main/java/me/dkim19375/utilities/api/UtilitiesAPI.java
+++ src/main/java/me/dkim19375/utilities/api/UtilitiesAPI.java
@@ -1,10 +1,6 @@
 package me.dkim19375.utilities.api;
 
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-
-import java.io.File;
-import java.util.HashMap;
 
 public class UtilitiesAPI {
     public static Player getPlayerFromAll(String uuidOrPlayer)  {
@@ -22,12 +18,4 @@ public class UtilitiesAPI {
     public static Player getPlayerFromUUID(String uuid) {
         return lgetPlayer.getPlayerFromUUID(uuid);
     }
-
-    public static boolean reloadConfig(String file, String pluginName, File configFile, FileConfiguration dataConfig) { return YMLApi.nreloadConfig(file, pluginName, configFile, dataConfig); }
-
-    public static FileConfiguration getConfig(FileConfiguration dataConfig) { return YMLApi.ngetConfig(dataConfig); }
-
-    public static boolean saveConfig(String file, String pluginName, File configFile, FileConfiguration dataConfig) { return YMLApi.nsaveConfig(file, pluginName, configFile, dataConfig); }
-
-    public static boolean saveDefaultConfig(String file, String pluginName, File configFile, HashMap<String, String> defaultConfig) { return YMLApi.nsaveDefaultConfig(file, pluginName, configFile, defaultConfig); }
 }
